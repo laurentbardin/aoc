c5091a2d608c938874ed4f6256821069b9de4f73
Laurent Bardin <laurent.bardin+gitlab@protonmail.com>
2023-12-06 13:51:06 +0100

    Add solution for day 1 part 1

---
+++ 2023/01/day01-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+calibration = []
+
+with open('calibration-1.txt', 'r') as content:
+    for line in content:
+        line.strip()
+
+        # Search first and last digit characters
+        first, last = 0, 0
+        for char in line:
+            if char.isdigit():
+                first = char
+                break
+    
+        for char in reversed(line):
+            if char.isdigit():
+                last = char
+                break
+    
+        calibration.append(int(first + last))
+
+print('Calibration:', sum(calibration))

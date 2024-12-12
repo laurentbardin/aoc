170ffd275652ff0e981779ee3b2e0703e773d85d
Laurent Bardin <laurent.bardin+gitlab@protonmail.com>
2024-12-12 18:11:05 +0100

    Add solution for day 11 part 1

---
+++ 2024/11/day11-1.py
@@ -0,0 +1,64 @@
+import sys
+import unittest
+
+def main():
+    filename = sys.argv[1]
+    if not filename:
+        usage()
+        sys.exit(1)
+
+    total_stones = data_from_file(filename, analyze_stones)
+    print(f"Final number of stones: {total_stones}")
+
+def usage():
+    print(f"Usage: python {sys.argv[0]} <filename>")
+
+def analyze_stones(file):
+    stones = [stone for stone in map(int, file.readline().strip().split())]
+
+    total = 0
+    for (idx, stone) in enumerate(stones):
+        total += 1
+        total += blink_stone(stone, 25)
+
+    return total
+
+def blink_stone(stone, iterations):
+    if iterations == 0:
+        return 0
+
+    added_stone = 0
+    if stone == 0:
+        added_stone += blink_stone(1, iterations - 1)
+    elif len(str(stone)) % 2 == 0:
+        s = str(stone)
+        size = len(s) // 2
+        added_stone = 1
+        for new_stone in [s[:size], s[size:]]:
+            added_stone += blink_stone(int(new_stone), iterations - 1)
+    else:
+        added_stone += blink_stone(stone * 2024, iterations - 1)
+
+    return added_stone
+
+def data_from_file(filename, cb):
+    try:
+        with open(filename, 'r') as file:
+            return cb(file)
+
+    except OSError as e:
+        print(f"Error reading {filename}: {e.strerror}")
+        usage()
+        sys.exit(1)
+
+class TestTopography(unittest.TestCase):
+
+    def test_trails(self):
+        stones = data_from_file('example.txt', analyze_stones)
+        self.assertEqual(stones, 55312)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    else:
+        main()

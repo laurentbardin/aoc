44bd13152b72cf508df27e9088c4828b701c9ddd
Laurent Bardin <laurent.bardin+gitlab@protonmail.com>
2024-12-12 17:26:39 +0100

    Add solution for day 10 part 2

--- 2024/10/day10-2.py
+++ 2024/10/day10-2.py
@@ -16,23 +16,17 @@ def usage():
     print(f"Usage: python {sys.argv[0]} <filename>")
 
 def analyze_map(file):
-    score = 0
+    rating = 0
 
     topography = Map(file)
     for (x, y) in topography.find_zeroes():
         #print(f"Looking at 0 @ ({x},{y})...")
-        trails_end = set()
-        #for candidate in topography.get_candidates(x, y):
-        #    print(f"Candidate: ({candidate[0]},{candidate[1]})")
         for (next_x, next_y) in topography.walk_trail(x, y):
-            if topography.slope_at(next_x, next_y) == 9:
-                #print(f"Got result from ({next_x},{next_y})")
-                trails_end.add((next_x, next_y))
-                #print(f"Current tally: {trails_end}")
+            #print(f"Got result from ({next_x},{next_y})")
+            rating += 1
+            #print(f"Current tally: {trails_end}")
 
-        score += len(trails_end)
-
-    return score
+    return rating
 
 def data_from_file(filename, cb):
     try:
@@ -47,8 +41,8 @@ def data_from_file(filename, cb):
 class TestTopography(unittest.TestCase):
 
     def test_trails(self):
-        score = data_from_file('example.txt', analyze_map)
-        self.assertEqual(score, 36)
+        rating = data_from_file('example.txt', analyze_map)
+        self.assertEqual(rating, 81)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
